32f782e7520c10e747baab0aed565d51c4e253bb
Mat Gadd <mat.gadd@advanceitgroup.co.uk>
2010-01-27 10:21:03 +0000

    Move the config file into the config directory, new class for logs etc.

--- core.php
+++ core.php
@@ -20,3 +20,51 @@ function log_msg() {
 
 	fwrite($log_file, $msg);
 }
+
+class Core {
+	protected static $levels = array(
+		'error' => 1,
+		'info' => 2,
+		'warning' => 3,
+		'debug' => 4,
+	);
+
+	public static function log($level, $message) {
+		echo 'Core::log(', $level, ', ', $message, ")\n";
+		// Check it's a valid level.
+		if (! array_key_exists($level, self::$levels))
+			return;
+
+		// Grab the level number.
+		$level_num = self::$levels[$level];
+
+		// Don't log if the level is above the configured one.
+		if ($level_num > config::item('config.log_level', 1))
+			return;
+
+		// Do printf-style formatting if required.
+		$args = array_slice(func_get_args(), 2);
+		if ((bool) count($args)) {
+			$message = vsprintf($message, $args);
+		}
+
+		// Build the mesage.
+		$message = sprintf(
+			'%s - [%s] %s',
+			date('Y-m-d H:i:s'),
+			$level,
+			$message
+		)."\n";
+
+		// Output if we're debugging.
+		if (config::item('config.debug_mode'))
+			echo $message;
+
+		// Finally write it to the log.
+		static $log_file;
+		if (! (bool) $log_file) {
+			$log_file = fopen('log.txt', 'a');
+		}
+		fwrite($log_file, $message);
+	}
+}
--- index.php
+++ index.php
@@ -10,16 +10,15 @@ require_once('classes/config.php');
 $feeds = config::item('config.feeds', array());
 
 define('ENABLE_CACHE', config::item('config.enable_cache'));
-define('DEBUG', config::item('config.debug_mode'));
 
-log_msg('Request from %s to fetch %d feeds', $_SERVER['REMOTE_ADDR'], count($feeds));
+Core::log('debug', 'Request from %s to fetch %d feeds', $_SERVER['REMOTE_ADDR'], count($feeds));
 
 $aggregate = new SimpleXMLElement('<?xml version="1.0" encoding="utf-8" ?><feed />');
 $aggregate->addChild('id', config::item('config.url'));
 $aggregate->addChild('title', config::item('config.title'));
 
 foreach ($feeds as $id => $feed) {
-	log_msg('Fetching %s', $id);
+	Core::log('info', 'Fetching %s', $id);
 
 	$id = str_replace(array('/', ' '), '_', $id);
 	$cache_file = sprintf('cache/feed_%s.xml', $id);
@@ -33,17 +32,17 @@ foreach ($feeds as $id => $feed) {
 			file_put_contents($cache_file, $data);
 	}
 
-	log_msg('Parsing %d bytes', strlen($data));
+	Core::log('debug', 'Parsing %d bytes', strlen($data));
 	
 	// Parse the feed.
 	$xml = simplexml_load_string($data);
 
-	log_msg('Appending data');
+	Core::log('debug', 'Appending data');
 	
 	foreach ($xml->entry as $entry) {
 		$node = $aggregate->addChild('entry');
 		foreach ($entry as $key => $value) {
-			log_msg('Appending key %s, data %s', (string) $key, (string) $value);
+			Core::log('debug', 'Appending key %s, data %s', (string) $key, (string) $value);
 			$subnode = $node->addChild((string) $key, str_replace('&', '&#38;', (string) $value));
 			foreach ($value->attributes() as $attrkey => $attrval) {
 				$subnode->addAttribute($attrkey, (string) $attrval);
@@ -52,11 +51,11 @@ foreach ($feeds as $id => $feed) {
 	}
 }
 
-if (defined('DEBUG') AND DEBUG)
+if (config::item('config.debug_mode'))
 	header('Content-Type: text/plain');
 else
 	header('Content-Type: application/atom+xml');
 
 echo $aggregate->asXML();
 
-log_msg('Finished refreshing');
+Core::log('info', 'Finished refreshing');
