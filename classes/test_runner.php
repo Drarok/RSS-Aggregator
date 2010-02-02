d65ff8d8d656513bf2165bbdcb704d84105acb91
Mat Gadd <mat.gadd@advanceitgroup.co.uk>
2010-02-02 11:52:39 +0000

    Some work on tests.

--- classes/test_runner.php
+++ classes/test_runner.php
@@ -1,15 +1,51 @@
 <?php
 
 class Test_Runner {
-	protected $test_files = array();
+	protected $tests = array();
 
 	public function __construct() {
+		$this->load();
 	}
 
 	protected function load() {
-		$test_files = glob(APPPATH.'tests/*.php');
+		foreach (glob(APPPATH.'tests/*.php') as $test_path) {
+			$class_name = $this->transform($test_path);
+			$this->tests[$class_name] = $test_path;
+		}
+	}
+
+	/**
+	 * Transform a class file path into its class name.
+	 * This is only valid for test cases, so it's not
+	 * available outside this class.
+	 */
+	protected function transform($class_path) {
+		// Remove the path part.
+		$class_name = basename($class_path, EXT);
+
+		// Explode on underscores.
+		$parts = explode('_', $class_name);
+
+		// Remove the leading numbers.
+		array_shift($parts);
+
+		// Add the trailing 'test'
+		array_push($parts, 'test');
+
+		// Uppercase the words and return.
+		return implode('_', array_map('ucfirst', $parts));
 	}
 
 	public function start() {
+		foreach ($this->tests as $class_name => $class_path) {
+			require_once($class_path);
+			$test = new $class_name();
+			try {
+				$test->run();
+			} catch (Exception $e) {
+				echo $e->getMessage(), "\n";
+			}
+			unset($test);
+		}
 	}
 }
--- tests/001_sqlite2.php
+++ tests/001_sqlite2.php
@@ -2,5 +2,6 @@
 
 class Sqlite2_Test extends Test_Case {
 	public function run() {
+		echo __FUNCTION__, "\n";
 	}
 }
