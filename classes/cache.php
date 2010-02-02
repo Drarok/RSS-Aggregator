97b336b4c053ad8e92c34842f6384878f23e53c7
Drarok Ithaqua <webmaster@drarok.com>
2010-02-02 02:36:33 +0000

    Add a cache class. Use that for RSS fetches. Add a config item for the above.

---
+++ classes/cache.php
@@ -0,0 +1,71 @@
+<?php
+
+class Cache {
+	protected static $enabled;
+	protected $path;
+
+	public static function init() {
+		self::$enabled = Core::config('config.cache_enabled', TRUE);
+		Core::log('debug', 'Initialised Cache. Enabled = %s', self::$enabled ? 'TRUE' : 'FALSE');
+	}
+
+	public function __construct($name) {
+		Core::log('debug', 'Creating new Cache object for %s', $name);
+		$name = str_replace('/', '_', $name);
+		$this->path = APPPATH.'cache/'.$name;
+	}
+
+	public function valid() {
+		// Should we even use the cache?
+		if (! self::$enabled)
+			return FALSE;
+
+		// Can't be valid unless it exists.
+		if (! file_exists($this->path))
+			return FALSE;
+
+		// Always assume an empty file is invalid.
+		if (! filesize($this->path))
+			return FALSE;
+
+		// Check the modification time isn't earlier than now() - expiry.
+		$mod_time = filemtime($this->path);
+		$now = time();
+		$expiry = Core::config('config.cache_expiry', 1200);
+		$earliest = $now - $expiry;
+
+		// Log detailed information in debug mode.
+		Core::log(
+			'debug',
+			'Cache file time is %s, earliest valid is %s',
+			date('Y-m-d H:i:s', $mod_time),
+			date('Y-m-d H:i:s', $earliest)
+		);
+
+		// Compare the values.
+		if ($mod_time < $earliest) {
+			Core::log('info', 'Expiring cache file %s', $this->path);
+			unlink($this->path);
+			return FALSE;
+		}
+
+		// Must be ok!
+		return TRUE;
+	}
+
+	public function read() {
+		if (! $this->valid())
+			return FALSE;
+
+		return file_get_contents($this->path);
+	}
+
+	public function write($data) {
+		if (! self::$enabled)
+			return;
+
+		file_put_contents($this->path, $data);
+	}
+}
+
+Cache::init();
--- classes/rss.php
+++ classes/rss.php
@@ -40,28 +40,13 @@ class RSS {
 	}
 
 	protected function get_rss_data() {
-		// Initialise vars.
-		$data = FALSE;
-		$cache_path = APPPATH.'cache/'.$this->name.'.xml';
+		// Use the cache if available.
+		$cache = new Cache($this->name.'.xml');
+		$data = $cache->read();
 
-		// Should we use the cache?
-		if (Core::config('config.enable_cache')) {
-			// Is there a cached result for this feed?
-			if (file_exists($cache_path)) {
-				Core::log('debug', 'Fetching %s RSS data from cache', $this->name);
-				$data = file_get_contents($cache_path);
-			}
-		}
-
-		// If nothing fetched, get the real data.
 		if (! (bool) $data) {
 			Core::log('debug', 'Fetching %s RSS data from %s', $this->name, $this->url);
-			$data = file_get_contents($this->url);
-
-			// Should we store this in the cache?
-			if (Core::config('config.enable_cache')) {
-				file_put_contents($cache_path, $data);
-			}
+			$cache->write($data = file_get_contents($this->url));
 		}
 
 		return $data;
