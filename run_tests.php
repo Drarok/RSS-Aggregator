8d2ebe78418e2a00d1a92eb607515c4e0c60ed5a
Drarok Ithaqua <webmaster@drarok.com>
2010-02-05 15:26:58 +0000

    Force the right log mode.

--- run_tests.php
+++ run_tests.php
@@ -6,6 +6,10 @@ require_once('classes/core.php');
 // Include this manually because it's a git submodule.
 require_once('classes/ansi-colors/ansi_color.php');
 
+// Force debug-level logging.
+Core::set_config('config.debug_mode', TRUE);
+Core::set_config('config.log_level', 4);
+
 // Start off a test runner.
 $runner = new Test_Runner();
 $runner->start();
