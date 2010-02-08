51732db5ba0634109e18815252bd9e103771ccec
Mat Gadd <mat.gadd@advanceitgroup.co.uk>
2010-02-08 11:40:59 +0000

    Coding style change.

--- run_tests.php
+++ run_tests.php
@@ -4,8 +4,9 @@
 require_once('classes/core.php');
 
 // Force at least info-level logging.
-if (Core::config('config.log_level') < 3)
+if (Core::config('config.log_level') < 3) {
 	Core::set_config('config.log_level', 3);
+}
 
 // Ensure we output to the console.
 Core::set_config('config.debug_mode', TRUE);
